ee2ef7ad6d36ad2ba884cbf600e92663d2641372
igorjba <igorjbahia@gmail.com>
2022-11-28 10:05:46 -0300

    :sparkles: feat: Exercício 10 resolvido.

---
+++ 10/index.js
@@ -0,0 +1,10 @@
+const numeros = [1, 98, -76, 0, 12, 19, 5, 60, 44]
+
+const verificarNumerosPares = ((listaDeNumeros) => {
+    const numerosPares = listaDeNumeros.filter((numero) => {
+        return numero % 2 === 0;
+    });
+    console.log(numerosPares);
+});
+
+verificarNumerosPares(numeros)
