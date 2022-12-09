775f13a27e17a19c62d41db503831003e762030c
igorjba <igorjbahia@gmail.com>
2022-12-09 16:50:14 -0300

    :recycle: refactor: Exercício refatorado.

--- 11/index.js
+++ 11/index.js
@@ -8,23 +8,8 @@ const nomes = [
     "amanda",
 ];
 
-const verificarNomes = ((listaDeNomes) => {
-
-    let nomesComA = [];
-
-    const verificarNomesComA = listaDeNomes.filter((nome) => {
-
-        comA = nome.indexOf("A", 0, 0)
-
-        coma = nome.indexOf("a", 0, 0)
-        if (comA === 0 || coma === 0) {
-            nomesComA.push(nome);
-        }
-
-    });
-
-    console.log(nomesComA);
-
+const verificarNomesComA = nomes.filter((nome) => {
+    return nome.slice(0, 1).toLowerCase() === "c";
 });
 
-verificarNomes(nomes)
+console.log(verificarNomesComA);
