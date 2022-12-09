d7055cf5ad0e4a07619c09e8b51e88b3ff1e9fad
igorjba <igorjbahia@gmail.com>
2022-12-09 16:44:03 -0300

    :recycle: refactor: Exercício refatorado.

--- 09/index.js
+++ 09/index.js
@@ -1,10 +1,6 @@
 const numeros = [10, 987, -886, 0, 12, 199, -45, -67]
 
-const verificarNumerosPositivos = ((listaDeNumeros) => {
-    const numerosPositivos = listaDeNumeros.filter((numero) => {
-        return numero > 0;
-    })
-    console.log(numerosPositivos);
+const numerosPositivos = numeros.filter((numero) => {
+    return numero > 0;
 })
-
-verificarNumerosPositivos(numeros)
+console.log(numerosPositivos);
